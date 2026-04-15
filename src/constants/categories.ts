45cb9ed236732aa66cc4ab09df2adb8ce7105101
seoCon - Online Marketing Agentur <office@seocon.at>
2026-04-15 10:42:15 +0200

    Create categories.ts

---
+++ src/constants/categories.ts
@@ -0,0 +1,95 @@
+export const CATEGORY_HIERARCHY: Record<string, string[]> = {
+  'Wirtschaft & Recht': [
+    'BWL', 'Recht', 'Business Administration', 'Business Management', 'Controlling',
+    'Entrepreneurship', 'Unternehmensführung', 'General Management', 'Kriminologie',
+    'Innovationsmanagement', 'Wirtschaftswissenschaften', 'Digital Business',
+    'Energiemanagement', 'Eventmanagement', 'Wirtschaftsrecht', 'Hotelmanagement',
+    'Volkswirtschaftslehre', 'IT Management', 'Internationales Management',
+    'Kulturmanagement', 'Immobilienmanagement', 'Wirtschaftsingenieurwesen',
+    'Internationale Beziehungen', 'Logistik', 'Unternehmensrecht', 'Tourismusmanagement',
+    'Wirtschaftspsychologie', 'Personalmanagement & HR', 'Master of Business Administration',
+    'Bank- und Finanzwesen', 'Versicherungen', 'Steuerwesen'
+  ],
+  'Technik & Ingenieurwissenschaft': [
+    'Bahntechnologie', 'Automatisierungstechnik', 'Energietechnik', 'Gebäudetechnik',
+    'Technische Wissenschaften', 'Technisches Management', 'Produktionsmanagement',
+    'Luftverkehrsmanagement', 'Fahrzeugtechnik', 'Materialwissenschaften', 'Tunnelbau',
+    'Industrial Engineering', 'Kunststofftechnik', 'Luft- und Raumfahrttechnik',
+    'Architektur', 'Holztechnologie', 'Maschinenbau', 'Elektrotechnik', 'Bauingenieurwesen'
+  ],
+  'Gesundheit, Medizin & Sport': [
+    'Medizin', 'Psychologie', 'Diätologie', 'eHealth', 'Gerontologie', 'Gesundheitsförderung',
+    'Pflegemanagement', 'Sportgerätetechnik', 'Veterinärmedizin', 'Gesundheitsmanagement',
+    'Musiktherapie', 'Physiotherapie', 'Ergotherapie', 'Ernährungswissenschaften',
+    'Ernährungsmedizin', 'Biomedizinische Analytik', 'Augenheilkunde', 'Neurowissenschaften',
+    'Orthopädie', 'Sportmedizin', 'Gesundheitstourismus', 'Radiologietechnologie',
+    'Sportmanagement', 'Biomedizin', 'Gesundheits- und Krankenpflege', 'Hebamme',
+    'Logopädie', 'Public Health', 'Medizintechnik', 'Sportwissenschaft', 'Psychotherapie',
+    'Krankenhausmanagement', 'Pharmazie', 'Pflegewissenschaften', 'Zahnmedizin',
+    'Lebensmittelwissenschaften'
+  ],
+  'Pädagogik, Verwaltung & Soziales': [
+    'Agrarpädagogik', 'Bildungsmanagement', 'Erziehungswissenschaften', 'Gesangspädagogik',
+    'Musikpädagogik', 'Gesundheitspädagogik', 'Pflegepädagogik', 'Sozialwissenschaften',
+    'Bildungswissenschaft', 'Coaching', 'Lehramt', 'Soziologie', 'Heilpädagogik',
+    'Medienpädagogik', 'Soziale Arbeit', 'Public Management', 'Instrumentalpädagogik',
+    'Wirtschaftspädagogik', 'Verkehrswesen', 'Sozialmanagement', 'Pädagogik',
+    'Lehramt Primarstufe', 'Polizeiwissenschaft', 'Sonderpädagogik', 'Lehramt Berufsbildung',
+    'Sozialpädagogik', 'Kindheitspädagogik'
+  ],
+  'Sprachen, Kultur- & Geisteswissenschaften': [
+    'Afrikawissenschaften', 'Ägyptologie', 'Alte Geschichte', 'Amerikanistik', 'Anglistik',
+    'Asienwissenschaften', 'Archäologie', 'Arabistik', 'Fennistik', 'Kunstgeschichte',
+    'Kunstwissenschaft', 'Geistenwissenschaften', 'Skandinavistik', 'Politikwissenschaft',
+    'Gender Studies', 'Ethik', 'Dolmetschen und Translatologie', 'Ethnologie',
+    'Theaterwissenschaft', 'Germanistik', 'Kulturwissenschaft', 'Philosophie',
+    'Religionswissenschaft', 'Neogräzistik und Byzantinistik', 'Geschichte', 'Slawistik',
+    'Romanistik', 'Anthropologie', 'Linguistik', 'Literaturwissenschaft',
+    'Sprachwissenschaft', 'Philologie', 'Judaistik', 'Sinologie', 'Hungarologie',
+    'Orientalistik', 'Theologie', 'Japanologie', 'Nederlandistik', 'Sprachen', 'European Studies'
+  ],
+  'Medien & Kommunikation': [
+    'Marketing', 'Digitale Medien', 'Film & Fernsehen', 'Medienkunst', 'Webdesign',
+    'Medieninformatik', 'Medienmanagement', 'Medienproduktion', 'Kommunikationswissenschaft',
+    'Mediendesign', 'Publizistik', 'Medienwissenschaften', 'Medientechnik', 'Public Relations',
+    'Kommunikationsmanagement', 'Journalismus', 'Wissensmanagement'
+  ],
+  'Informatik & Mathematik': [
+    'Artificial Intelligence', 'Computer Science', 'Data Science', 'Embedded Systems',
+    'Game Design', 'Wirtschaftsinformaik', 'Medizinische Informatik', 'Informationstechnik (IT)',
+    'E-Commerce', 'Informatik', 'Informationsmanagement', 'IT Security', 'Mechatronik',
+    'Mathematik', 'Technische Mathematik', 'Software Engineering', 'Technische Informatik'
+  ],
+  'Design, Kunst & Musik': [
+    'Bildende Kunst', 'Bühnenbild', 'Dirigieren', 'Gesang', 'Kunst', 'Fotografie',
+    'Industrial Design', 'Tanz', 'Grafikdesign', 'Webdesign', 'Komposition',
+    'Instrumentalstudium', 'Konservierung & Restaurierung', 'Mode', 'Musikwissenschaft',
+    'Innenarchitektur', 'Schauspiel', 'Kommunikationsdesign', 'Korrepetition',
+    'Musikmanagement', 'Kirchenmusik', 'Jazz'
+  ],
+  'Naturwissenschaften': [
+    'Astronomie', 'Biotechnologie', 'Biophysik', 'Chemie', 'Biologie', 'Forensik',
+    'Statistik', 'Geographie', 'Biochemie', 'Bioinformatik', 'Zoologie', 'Geodäsie',
+    'Geologie', 'Mathematik', 'Molekularbiologie', 'Physik', 'Naturwissenschaften',
+    'Geowissenschaften'
+  ],
+  'Umwelt- & Agrarwissenschaften': [
+    'Agrarmanagement', 'Atmosphärenwissenschaften', 'Botanik', 'Erdwissenschaften',
+    'Umweltmanagement', 'Stadt- & Regionalentwicklung', 'Erneuerbare Energien',
+    'Umweltsystemwissenschaften', 'Rohstoffmanagement & -technik', 'Umwelttechnik',
+    'Agrarwissenschaften', 'Agrartechnologie', 'Forstwirtschaft', 'Umweltwissenschaften',
+    'Nachhaltigkeit', 'Pferdewissenschaften', 'Önologie'
+  ]
+};
+
+export const DEGREE_MAPPING: Record<string, string[]> = {
+  'Bachelor': ['bsc', 'b.sc', 'bachelor'],
+  'Master': ['ma', 'msc', 'm.sc', 'master'],
+  'Doktor': ['dr', 'doktor', 'doctor', 'phd'],
+  'Diplom/Ing.': ['ing', 'ingenieur', 'diplom'],
+  'Magister': ['magister', 'mag.'],
+  'Zertifikat': ['certificate', 'zertifikat', 'zerti'],
+  'Akademie': ['akademisch'],
+  'Coach': ['coach'],
+  'Lehrgang': ['lehrgang']
+};
